0a6a31f50b00eb570ae5ffcba4407d03c20019b8
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2018-10-15 09:57:59 -0500

    More installs

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,11 +2,22 @@
 echo "Installing by brew"
 brew install tree
 brew install neovim
-brew install tree
+brew install zsh
+brew install httpie
+brew install docker
+brew install docker-machine
+brew install minikube
+brew install mongodb
+brew install tmux
+brew install wget
+brew install azure-cli
+brew install tig
 brew install getantibody/tap/antibody
 brew cask install atom
 brew cask install firefox
 brew cask install intellij-idea
+brew cask install iterm2
+brew cask install virtualbox
 brew cask install slack
 brew cask install spotify
 echo "Done"
